bc5be47d5213c6984a55c758ca9365278f006600
Nagalakshmi S <nagalakshmi6689@gmail.com>
2022-09-13 21:36:33 +0530

    changes done

--- table creation.sql
+++ table creation.sql
@@ -1,8 +1,10 @@
 create database CaseStudy;
 use CaseStudy;
-create table if not exists productCatalog(
-id BIGINT primary key,
+create table if not exists ProductCatalog(
+id int primary key,
 code varchar(255),
 name varchar(255),
 description varchar(255),
 price double);
+drop table ProductCatalog;
+insert ignore into ProductCatalog values(1,"PRO01","product1","related to product",350);
