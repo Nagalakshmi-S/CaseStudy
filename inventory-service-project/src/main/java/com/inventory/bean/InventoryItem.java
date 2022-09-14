b4d98b5ed567b46afd5e1077baecd09f9c6a1923
Nagalakshmi S <nagalakshmi6689@gmail.com>
2022-09-14 16:29:01 +0530

    inventory service

---
+++ inventory-service-project/src/main/java/com/inventory/bean/InventoryItem.java
@@ -0,0 +1,22 @@
+package com.inventory.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class InventoryItem {
+	@Id
+    private Long id; 
+    private String productCode;
+    private Integer availableQuantity ;
+}
+
+
+
--- table creation.sql
+++ table creation.sql
@@ -7,4 +7,6 @@ name varchar(255),
 description varchar(255),
 price double);
 drop table ProductCatalog;
-insert ignore into ProductCatalog values(1,"PRO01","product1","related to product",350);
+insert ignore into Product values(1,"P001","Product1","This is 1st product",778);
+select * from productcatalog;
+truncate productcatalog;
